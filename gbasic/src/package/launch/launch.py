dc3618d2be7e7b94b49b9b36252c0cbd55999829
sathvik0320 <sathvik0320@gmail.com>
2024-08-30 00:45:47 +0530

    basic model simulation in gzebo

--- gbasic/src/package/launch/launch.py
+++ gbasic/src/package/launch/launch.py
@@ -22,11 +22,8 @@ def generate_launch_description():
 
     #package path to urdf and launch
     path = "urdf/robo.urdf"
-    worldpath = "urdf/file.world"
     #actual path to urdf file
     actual = os.path.join(get_package_share_directory(packagename),path)
-    #path to world file actual path
-    wactual =os.path.join(get_package_share_directory(packagename),worldpath)
     #getting robot description from urdf file
     with open(actual,"r") as file:
        x=file.read()
@@ -36,8 +33,8 @@ def generate_launch_description():
     #starting servers for gazebo gzserver and gzclient
     gazebolaunch = PythonLaunchDescriptionSource(os.path.join(get_package_share_directory("gazebo_ros"),"launch","gazebo.launch.py"))
     #if you want to include a specific world into gazebo
-    #gazebo = IncludeLaunchDescription(gazebolaunch,launch_arguments={}.items())
-    gazebo =IncludeLaunchDescription(gazebolaunch,launch_arguments={"world":wactual}.items())
+    #gazebo = IncludeLaunchDescription(gazebolaunch,launch_arguments={"world":pathfile}.items())
+    gazebo =IncludeLaunchDescription(gazebolaunch)
     #robot_state_pulisher node
     robotstate = launch_ros.actions.Node(
                   package="robot_state_publisher",

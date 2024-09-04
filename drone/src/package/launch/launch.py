ed06bf25d41e4670785c040769466f003756a55d
sathvik0320 <sathvik0320@gmail.com>
2024-09-04 18:42:49 +0530

    spawned into gazebo

--- drone/src/package/launch/launch.py
+++ drone/src/package/launch/launch.py
@@ -14,11 +14,17 @@ def generate_launch_description():
    packagename="package"
    robotname="drone"
    urdf_path = os.path.join(get(packagename),"urdf/drone.urdf")
+   world_path = os.path.join(get(packagename),"urdf/file.world")
+
    with open(urdf_path) as file :
            x=file.read()
            print("urdf"+ str(x))
    robot_info = {"robot_description":x}
-   gui=Declare(name="gui",default_value="True",description="with gui or without gui")
+
+   gui=Declare(name="gui",default_value="False",description="with gui or without gui")
+   rvi =Declare(name="rvi",default_value="False",description="condition for rviz and jointstate pulisher")
+   joint = Declare(name="joint",default_value="False",description="for join states")
+
    #creating the nodes
    robotstate =lr.actions.Node(
                package="robot_state_publisher",
@@ -26,31 +32,46 @@ def generate_launch_description():
                output="screen",
                parameters=[robot_info])
 
-   jointstate=lr.actions.Node(
-               package="joint_state_publisher",
-               executable="joint_state_publisher",
-               name="joint_state_publisher",
-               condition= l.conditions.UnlessCondition(input("gui")))
-   jointgui = lr.actions.Node(
-               package="joint_state_publisher_gui",
-               executable="joint_state_publisher_gui",
-               output="screen",
-               name="joint_state_publisher",
-               condition = l.conditions.IfCondition(input("gui"))
-               )
+   #jointstate=lr.actions.Node(
+   #            package="joint_state_publisher",
+   #            executable="joint_state_publisher",
+   #            name="joint_state_publisher",
+   #            condition= l.conditions.IfCondition(input("joint")))
+   #jointgui = lr.actions.Node(
+   #            package="joint_state_publisher_gui",
+   #            executable="joint_state_publisher_gui",
+   #            output="screen",
+   #            name="joint_state_publisher",
+   #            condition = l.conditions.IfCondition(input("gui")))
 
    rvizlaunch =lr.actions.Node(
                 package="rviz2",
                 executable="rviz2",
                 name="rviz2",
-                output="screen")
+                output="screen",
+                condition = l.conditions.IfCondition(input("rvi"))
+                )
 
+   gazebo = psource(os.path.join(get("gazebo_ros"),"launch","gazebo.launch.py"))
+   gazebolaunch = Include(gazebo,launch_arguments={"world":world_path}.items())
+
+   spawning = lr.actions.Node(
+               package="gazebo_ros",
+               executable="spawn_entity.py",
+               arguments=["-topic","robot_description","-entity",robotname],
+               output ="screen")
 
    object =Launch()
-   object.add_action(gui)
+   object.add_action(gazebolaunch)
    object.add_action(robotstate)
-   object.add_action(jointstate)
-   object.add_action(jointgui)
+   object.add_action(spawning)
+
+   object.add_action(gui)
+   object.add_action(rvi)
+   object.add_action(joint)
+
+   #object.add_action(jointstate)
+   #object.add_action(jointgui)
    object.add_action(rvizlaunch)
 
    return object
